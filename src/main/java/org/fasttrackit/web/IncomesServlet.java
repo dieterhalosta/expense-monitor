33476b581d032ee8d78962c84af13ff6c10684ce
Dieter Halosta <dieter.halosta@yahoo.com>
2020-05-26 13:40:51 +0300

    Implemented webservlets.

--- src/main/java/org/fasttrackit/web/IncomesServlet.java
+++ src/main/java/org/fasttrackit/web/IncomesServlet.java
@@ -2,8 +2,11 @@ package org.fasttrackit.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.fasttrackit.config.ObjectMapperConfiguration;
+import org.fasttrackit.domain.Incomes;
 import org.fasttrackit.service.IncomesService;
 import org.fasttrackit.transfer.CreateIncomeRequest;
+import org.fasttrackit.transfer.UpdateIncomeRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet("/incomes")
 public class IncomesServlet extends HttpServlet {
@@ -20,17 +24,50 @@ public class IncomesServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
 
-        CreateIncomeRequest request = objectMapper.readValue(req.getReader(), CreateIncomeRequest.class);
+        CreateIncomeRequest request = ObjectMapperConfiguration.OBJECT_MAPPER.readValue(req.getReader(), CreateIncomeRequest.class);
 
         try {
             incomesService.createIncome(request);
         } catch (SQLException | ClassNotFoundException e) {
             resp.sendError(500, "There was an error and couldn't reach the server. " + e.getMessage());
         }
+    }
 
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+
+        UpdateIncomeRequest request = ObjectMapperConfiguration.OBJECT_MAPPER.readValue(req.getReader(), UpdateIncomeRequest.class);
+
+        try {
+            incomesService.updateIncome(Long.parseLong(id), request);
+        } catch (SQLException | ClassNotFoundException e) {
+            resp.sendError(500, "There was an error and couldn't reach the server. " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        try {
+            incomesService.deleteIncome(Long.parseLong(id));
+        } catch (SQLException | ClassNotFoundException e) {
+            resp.sendError(500, "There was an error and couldn't reach the server. " + e.getMessage());
+        }
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            List<Incomes> incomes = incomesService.getIncomes();
+
+            ObjectMapperConfiguration.OBJECT_MAPPER.writeValue(resp.getWriter(), incomes);
+
+        } catch (SQLException | ClassNotFoundException e) {
+            resp.sendError(500, "There was an error and couldn't reach the server. " + e.getMessage());
+        }
     }
 }
 
--- src/main/java/org/fasttrackit/web/PaymentsServlet.java
+++ src/main/java/org/fasttrackit/web/PaymentsServlet.java
@@ -1,9 +1,12 @@
 package org.fasttrackit.web;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import org.fasttrackit.config.ObjectMapperConfiguration;
+import org.fasttrackit.domain.Payments;
 import org.fasttrackit.service.PaymentsService;
 import org.fasttrackit.transfer.CreatePaymentRequest;
+
+import org.fasttrackit.transfer.UpdatePaymentRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet("/payments")
 public class PaymentsServlet extends HttpServlet {
@@ -20,16 +24,48 @@ public class PaymentsServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
 
-        CreatePaymentRequest request = objectMapper.readValue(req.getReader(), CreatePaymentRequest.class);
+        CreatePaymentRequest request = ObjectMapperConfiguration.OBJECT_MAPPER.readValue(req.getReader(), CreatePaymentRequest.class);
 
         try {
             paymentsService.createPayment(request);
         } catch (SQLException | ClassNotFoundException e) {
             resp.sendError(500, "There was an error while trying to reach the server." + e.getMessage());
         }
+    }
 
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+
+        UpdatePaymentRequest request = ObjectMapperConfiguration.OBJECT_MAPPER.readValue(req.getReader(), UpdatePaymentRequest.class);
+
+        try {
+            paymentsService.updatePayment(Long.parseLong(id), request);
+        } catch (SQLException | ClassNotFoundException e) {
+            resp.sendError(500, "There was an error while trying to reach the server." + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+
+        try {
+            paymentsService.deletePayment(Long.parseLong(id));
+        } catch (SQLException | ClassNotFoundException e) {
+            resp.sendError(500, "There was an error while trying to reach the server." + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            List<Payments> payments = paymentsService.getPayments();
+
+            ObjectMapperConfiguration.OBJECT_MAPPER.writeValue(resp.getWriter(), payments);
+        } catch (SQLException | ClassNotFoundException e) {
+            resp.sendError(500, "There was an error while trying to reach the server." + e.getMessage());
+        }
     }
 }
